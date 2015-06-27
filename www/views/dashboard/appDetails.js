201804213375283b3fefd678a59f9e120569e64b
yunfeng97 <yunfeng97@gmail.com>
2015-06-27 13:36:33 -0400

    checkin

--- www/views/dashboard/appDetails.js
+++ www/views/dashboard/appDetails.js
@@ -3,11 +3,11 @@ angular.module('App')
         // return the details of an appointment with appontmentId
         $scope.getAppDetails = function () {
             var appid = $stateParams.appid;
-            console.log("app id is: " + $stateParams.appid);
+            //console.log("app id is: " + $stateParams.appid);
             console.log("app id is: " + appid);
             $scope.appid = appid;
             $http.get('/api/dashboard/app/' + appid).success(function (data) {
-                //$http.get('http://192.168.1.171:1337/api/dashboard/all').success(function (data) {
+            //$http.get('http://192.168.1.171:1337/api/dashboard/app/2').success(function (data) {
                 $scope.appDetails = data;
                 console.log("in app detail controller");
                 //return data;
@@ -19,7 +19,30 @@ angular.module('App')
             });
         };
 
-        //$scope.getAppDetails();
+        $scope.accept = function(appid){
+            //console.log("in accept");
+            $http.put('/api/dashboard/app/accept/:id' + appid).success(function (data) {
+            //$http.put('http://192.168.1.171:1337/api/dashboard/app/accept/2').success(function (data) {
+                $scope.appDetails = data;
+                console.log("data: " + data);
+                console.log("in app detail controller");
+                //return data;
+                //}).error(function (error) {
+                //    $scope.error = error;
+                //    console.log(error);
+            }).error(function (data, status, headers, config) {
+                console.log("Error occurred.  Status:" + status);
+                console.log("Error occurred.  Data:" + data);
+            }).finally(function () {
+                $scope.$broadcast('scroll.refreshComplete')
+            });
+        };
+
+        $scope.reschedule = function(){
+
+        };
+
+        $scope.getAppDetails();
 
         //if (angular.isUndefined($scope.appDetails)) {
         //if ($scope.appDetails == null ) {
--- www/views/dashboard/dashboard.js
+++ www/views/dashboard/dashboard.js
@@ -28,7 +28,7 @@ angular.module('App')
             //$http.get('/api/dashboard/all', {params: {userId: userId}}).success(function (data) {
             //$http.get(ApiEndpoint.url + '/dashboard/all').success(function (data) {
             $http.get('/api/dashboard/todayApps').success(function (data) {
-                //$http.get('http://192.168.1.171:1337/api/dashboard/all').success(function (data) {
+            //$http.get('http://192.168.1.171:1337/api/dashboard/todayApps').success(function (data) {
                 $scope.todayApps = data;
                 console.log(data);
                 //return data;
@@ -74,23 +74,23 @@ angular.module('App')
         };
 
         // return the details of an appointment with appontmentId
-        $scope.getAppDetails = function (appid) {
-            //console.log("app id is: " + $stateParams.appid);
-            console.log("app id is: " + appid);
-            $scope.appid = appid;
-            $http.get('/api/dashboard/app/' + appid).success(function (data) {
-                //$http.get('http://192.168.1.171:1337/api/dashboard/all').success(function (data) {
-
-                $scope.appid = appid;
-                console.log("in app details");
-                //return data;
-            }).error(function (error) {
-                $scope.error = error;
-                console.log(error);
-            }).finally(function () {
-                $scope.$broadcast('scroll.refreshComplete')
-            });
-        };
+        //$scope.getAppDetails = function (appid) {
+        //    //console.log("app id is: " + $stateParams.appid);
+        //    console.log("app id is: " + appid);
+        //    $scope.appid = appid;
+        //    //$http.get('/api/dashboard/app/' + appid).success(function (data) {
+        //    $http.get('http://192.168.1.171:1337/api/dashboard/app/2').success(function (data) {
+        //
+        //        $scope.appid = appid;
+        //        console.log("in app details");
+        //        //return data;
+        //    }).error(function (error) {
+        //        $scope.error = error;
+        //        console.log(error);
+        //    }).finally(function () {
+        //        $scope.$broadcast('scroll.refreshComplete')
+        //    });
+        //};
 
         $scope.getTodayApps("");
     });
