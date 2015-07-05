dbeb56f936d998943756868363d67c1504d4db0a
yunfeng97 <yunfeng97@gmail.com>
2015-07-05 10:06:18 -0400

    checkin

--- www/views/clients/clients.js
+++ www/views/clients/clients.js
@@ -1,3 +1,21 @@
 /**
  * Created by yunfeng on 02/07/15.
  */
+angular.module('App')
+    .controller('ClientsController', function ($scope, $http, $stateParams) {
+
+        $scope.getAllClients = function () {
+            $http.get('/api/clients/allClients').success(function (data) {
+                //$http.get('http://192.168.1.171:1337/api/clients/allClients').success(function (data) {
+                $scope.allClients = data;
+                //console.log(data);
+                //return data;
+            }).error(function (error) {
+                $scope.error = error;
+                console.log(error);
+            }).finally(function () {
+                $scope.$broadcast('scroll.refreshComplete')
+            });
+        };
+
+    });
